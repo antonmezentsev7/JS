a1a95ea2a3575602499eae813e9bb2095d02a23a
Антон Мезенцев <a.mezentcev@oshara.CEK.local>
2021-04-19 12:25:00 +0300

    add_methods

--- js/script.js
+++ js/script.js
@@ -52,4 +52,73 @@ class ProductItem {
     }
 }
 
+class BusketList {
+    constructor(container = '.busket__container') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this.counts = this._getBusketCounts()
+            .then(data => {
+                let amountBlock = document.querySelector(".amount");
+                let countBlock = document.querySelector(".count");
+                amountBlock.insertAdjacentHTML("beforeend", data.amount);
+                countBlock.insertAdjacentHTML("beforeend", data.countGoods);
+            });
+        this._getBusketProducts()
+            .then(data => {
+                this.goods = [...data.contents];
+                this.render()
+            });
+    };
+
+    _getBusketCounts() {
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    _getBusketProducts() {
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new BusketItem(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+        }
+
+    }
+}
+
+
+class BusketItem {
+    constructor(product, img = 'https://placehold.it/200x150') {
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id_product;
+        this.img = img;
+        this.quantity = product.quantity;
+    }
+    render() {
+        return `<div class="busket-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} $</p>
+                    <p>Количество<br><button>-</button> ${this.quantity} <button>+</button></p>
+                </div>
+            </div>`
+    }
+
+
+}
+
 let list = new ProductsList();
